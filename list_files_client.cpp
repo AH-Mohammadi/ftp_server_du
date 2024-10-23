741320d204b8ae1a007321ea07d47ec737da6a71
Amirhossein Mohammadi <123809063+AH-Mohammadi@users.noreply.github.com>
2024-10-23 12:53:22 +0330

    Update and rename file_list_client.cpp to list_files_client.cpp

--- list_files_client.cpp
+++ list_files_client.cpp
@@ -9,43 +9,31 @@ int main() {
     int sock = 0;
     struct sockaddr_in serv_addr;
 
-    // Create a socket
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        std::cerr << "Socket creation error\n";
+        std::cerr << "Error creating socket. Exiting...\n";
         return -1;
     }
 
-    // Configure the server address
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    // Convert IPv4 address from text to binary form
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        std::cerr << "Invalid address/Address not supported\n";
+        std::cerr << "Invalid server address. Exiting...\n";
         return -1;
     }
 
-    // Connect to the server
     if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        std::cerr << "Connection failed\n";
+        std::cerr << "Failed to connect to the server. Try again later.\n";
         return -1;
     }
 
-    // Send the command to get the file list
     const char* command = "GET_FILE_LIST";
     send(sock, command, strlen(command), 0);
 
-    // Receive the file list from the server
-    char buffer[1024] = {0};
+    char buffer[2048] = {0};
     int bytes_read = read(sock, buffer, sizeof(buffer));
     if (bytes_read > 0) {
-        std::cout << "File list received from server:\n";
+        std::cout << "Files available on the server:\n";
         std::cout << buffer;
     } else {
-        std::cerr << "Failed to receive file list\n";
-    }
-
-    // Close the socket
-    close(sock);
-    return 0;
-}
+        std
